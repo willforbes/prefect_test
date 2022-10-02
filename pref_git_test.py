6932a7cc5ed1464373ab1c3d156601fe7d5e9d3e
willforbes <forbeswilliam@hotmail.com>
2022-10-02 22:13:02 +0100

    Add log of path file is running from

--- pref_git_test.py
+++ pref_git_test.py
@@ -1,6 +1,8 @@
 from prefect import flow, get_run_logger
+import os
 
 @flow(name="log-example-flow")
 def logger_flow():
     logger = get_run_logger()
     logger.info("INFO level log message.")
+    logger.info(os.path.realpath(__file__))
